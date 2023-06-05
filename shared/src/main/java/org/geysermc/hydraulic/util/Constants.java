1ac70a7d3a1e819b121f7d488cd93c13d85097ef
RednedEpic <redned235@gmail.com>
2023-06-04 23:11:50 -0500

    Remove lingering jackson references

--- shared/src/main/java/org/geysermc/hydraulic/util/Constants.java
+++ shared/src/main/java/org/geysermc/hydraulic/util/Constants.java
@@ -1,12 +1,6 @@
 package org.geysermc.hydraulic.util;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class Constants {
-    public static final ObjectMapper MAPPER = new ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
     public static final String JAVA_TEXTURE_LOCATION = "assets/%s/textures/%s.png";
     public static final String JAVA_ITEM_MODEL_LOCATION = "assets/%s/models/item/%s.json";
 
