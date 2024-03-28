0348bca4915ab5888ae3892119949ac73d292494
rtm516 <ryantmilner@hotmail.co.uk>
2024-03-28 09:24:23 +0000

    Drop attempt at converting forge blockstates and just ignore

--- shared/src/main/java/org/geysermc/hydraulic/mixin/ext/BlockStateSerializerMixin.java
+++ shared/src/main/java/org/geysermc/hydraulic/mixin/ext/BlockStateSerializerMixin.java
@@ -1,16 +1,16 @@
 package org.geysermc.hydraulic.mixin.ext;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import net.kyori.adventure.key.Key;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import team.unnamed.creative.blockstate.BlockState;
 import team.unnamed.creative.serialize.minecraft.blockstate.BlockStateSerializer;
-
-import java.util.Map;
 
 @Mixin(value = BlockStateSerializer.class, remap = false)
 
@@ -18,71 +18,21 @@ public class BlockStateSerializerMixin {
     private static Logger LOGGER = LoggerFactory.getLogger("BlockStateSerializerMixin");
 
     /**
-     * This mixin attempts to convert forge_marker blockstates with variants to multipart blockstates
+     * This mixin prevents loading of the old forge_marker blockstates format as they are no longer supported
+     * <p>
+     * forge_marker was removed in 1.15 see <a href="https://docs.minecraftforge.net/en/1.14.x/models/blockstates/forgeBlockstates/">here</a> for more info
      */
-    @Redirect(
+    @Inject(
         method = "deserializeFromJson(Lcom/google/gson/JsonElement;Lnet/kyori/adventure/key/Key;)Lteam/unnamed/creative/blockstate/BlockState;",
-        at = @At(
-            value = "INVOKE",
-            target = "Lcom/google/gson/JsonElement;getAsJsonObject()Lcom/google/gson/JsonObject;"
-        )
+        at = @At("HEAD"),
+        cancellable = true
     )
-    private JsonObject deserializeFromJson(JsonElement node) {
+    private void deserializeFromJson(JsonElement node, Key key, CallbackInfoReturnable<BlockState> cir) {
         JsonObject objectNode = node.getAsJsonObject();
-
-        // Only handle forge_marker blockstates with variants
-        if (!objectNode.has("forge_marker") || !objectNode.has("variants")) {
-            return objectNode;
+        if (objectNode.has("forge_marker")) {
+            // This was removed from forge in 1.15
+            LOGGER.warn("Ignoring forge_marker blockstate: " + key);
+            cir.setReturnValue(null);
         }
-
-        // Check if multipart exists and get it else create a new one
-        JsonArray multiPartNode = new JsonArray();
-        if (objectNode.has("multipart")) {
-            multiPartNode = objectNode.get("multipart").getAsJsonArray();
-        }
-
-        // Won't work for all cases, but should work for most
-        // TODO better edge case handling
-        JsonObject variantsNode = objectNode.getAsJsonObject("variants");
-        for (Map.Entry<String, JsonElement> variantEntry : variantsNode.entrySet()) {
-            JsonArray variantEntryArr = variantEntry.getValue().getAsJsonArray();
-
-            if (variantEntryArr.size() > 1) {
-                LOGGER.warn("More than 1 variant entry found!"); // TODO Add more info to this warning
-            }
-
-            // Only handle the first variant entry
-            JsonObject variantEntryObj = variantEntryArr.get(0).getAsJsonObject();
-            for (Map.Entry<String, JsonElement> variantDataEntry : variantEntryObj.entrySet()) {
-                JsonObject multipartApply;
-                JsonObject multipartWhen = new JsonObject();
-
-                // When condition
-                multipartWhen.addProperty(variantEntry.getKey(), variantDataEntry.getKey());
-
-                JsonObject variantDataEntryObj = variantDataEntry.getValue().getAsJsonObject();
-                if (!variantDataEntryObj.has("submodel")) {
-                    LOGGER.warn("No submodel found!"); // TODO Add more info to this warning
-                    continue;
-                }
-
-                // TODO Handle multiple submodels?
-                JsonObject submodelObj = variantDataEntryObj.get("submodel").getAsJsonObject();
-                multipartApply = submodelObj.get(submodelObj.keySet().toArray(new String[0])[0]).getAsJsonObject();
-
-                JsonObject multipartEntry = new JsonObject();
-                multipartEntry.add("apply", multipartApply);
-                multipartEntry.add("when", multipartWhen);
-                multiPartNode.add(multipartEntry);
-            }
-        }
-
-        // Remove the now-parsed variants
-        objectNode.remove("variants");
-
-        // Update multipart with the new entries
-        objectNode.add("multipart", multiPartNode);
-
-        return objectNode;
     }
 }
