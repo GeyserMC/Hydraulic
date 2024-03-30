c74a7b2d193686143b16a855fae61f766098b389
rtm516 <ryantmilner@hotmail.co.uk>
2024-03-30 12:53:14 +0000

    Fix pickblock for 2d item blocks

---
+++ shared/src/main/java/org/geysermc/hydraulic/mixin/ext/InventoryUtilsMixin.java
@@ -0,0 +1,66 @@
+package org.geysermc.hydraulic.mixin.ext;
+
+import com.github.steveice10.mc.protocol.data.game.entity.player.GameMode;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
+import org.geysermc.geyser.registry.type.ItemMapping;
+import org.geysermc.geyser.registry.type.ItemMappings;
+import org.geysermc.geyser.session.GeyserSession;
+import org.geysermc.geyser.util.InventoryUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = InventoryUtils.class, remap = false)
+public class InventoryUtilsMixin {
+
+    /**
+     * Find or create the item with the 2d item identifier
+     * Only for non-creative inventory items
+     */
+    @Inject(
+        method = "findOrCreateItem(Lorg/geysermc/geyser/session/GeyserSession;Ljava/lang/String;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lorg/geysermc/geyser/GeyserLogger;debug(Ljava/lang/String;)V"
+        ),
+        cancellable = true
+    )
+    private static void findOrCreateItemCreative(GeyserSession session, String itemName, CallbackInfo ci, @Share("creative") LocalBooleanRef creative) {
+        // If we are already looking for the `item.`, don't do anything
+        if (itemName.contains(":item.")) {
+            return;
+        }
+
+        // Flag this call as creative
+        creative.set(true);
+
+        InventoryUtils.findOrCreateItem(session, itemName.replace(":", ":item."));
+
+        ci.cancel();
+    }
+
+    /**
+     * Find or create the item with the 2d item identifier
+     * Only for non-creative inventory items
+     */
+    @Inject(
+        method = "findOrCreateItem(Lorg/geysermc/geyser/session/GeyserSession;Ljava/lang/String;)V",
+        at = @At("TAIL")
+    )
+    private static void findOrCreateItemSurvival(GeyserSession session, String itemName, CallbackInfo ci, @Share("creative") LocalBooleanRef creative) {
+        // Creative is handled in the previous mixin
+        if (creative.get()) {
+            return;
+        }
+
+        // If we are already looking for the `item.`, don't do anything
+        if (itemName.contains(":item.")) {
+            return;
+        }
+
+        InventoryUtils.findOrCreateItem(session, itemName.replace(":", ":item."));
+    }
+}
