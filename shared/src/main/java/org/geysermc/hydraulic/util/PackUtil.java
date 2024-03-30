b39804ac2798c2599688fed42358845dd671da5c
rtm516 <ryantmilner@hotmail.co.uk>
2024-03-30 13:19:39 +0000

    Fix pack uuid generation

--- shared/src/main/java/org/geysermc/hydraulic/util/PackUtil.java
+++ shared/src/main/java/org/geysermc/hydraulic/util/PackUtil.java
@@ -4,11 +4,6 @@ import com.google.common.hash.Hashing;
 import com.google.common.hash.HashingOutputStream;
 import com.mojang.logging.LogUtils;
 import net.kyori.adventure.key.Key;
-import org.geysermc.hydraulic.platform.mod.ModInfo;
-import org.geysermc.pack.bedrock.resource.BedrockResourcePack;
-import org.geysermc.pack.bedrock.resource.Manifest;
-import org.geysermc.pack.bedrock.resource.manifest.Header;
-import org.geysermc.pack.bedrock.resource.manifest.Modules;
 import org.geysermc.pack.converter.converter.texture.TextureMappings;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -19,7 +14,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -29,39 +23,6 @@ import java.util.stream.Stream;
  */
 public class PackUtil {
     protected static final Logger LOGGER = LogUtils.getLogger();
-    private static final int FORMAT_VERSION = 2;
-
-    /**
-     * Initializes a resource pack for the specified mod.
-     *
-     * @param mod the mod to initialize the resource pack for
-     * @return the initialized resource pack
-     */
-    public static BedrockResourcePack initializeForMod(@NotNull ModInfo mod, @NotNull Path packPack) {
-        Manifest manifest = new Manifest();
-        manifest.formatVersion(FORMAT_VERSION);
-
-        Header header = new Header();
-        header.description("Resource pack for mod " + mod.name());
-        header.name(mod.name() + " Resource Pack");
-        header.version(new float[] { 1, 0, 0 });
-        header.minEngineVersion(new float[] { 1, 16, 0 });
-        header.uuid(UUID.randomUUID().toString());
-
-        manifest.header(header);
-
-        Modules module = new Modules();
-        module.description("Resource pack for mod " + mod.name());
-        module.type("resources");
-        module.uuid(UUID.randomUUID().toString());
-        module.version(new float[] { 1, 0, 0 });
-
-        manifest.modules(List.of(module));
-
-        BedrockResourcePack pack = new BedrockResourcePack(packPack);
-        pack.manifest(manifest);
-        return pack;
-    }
 
     public static String getTextureName(@NotNull String modelName) {
         if (modelName.startsWith(Key.MINECRAFT_NAMESPACE)) {
@@ -91,7 +52,7 @@ public class PackUtil {
     public static UUID getModUUID(Collection<Path> modRoots) {
         final HashingOutputStream hos = new HashingOutputStream(Hashing.murmur3_128(), OutputStream.nullOutputStream());
         try (Stream<Path> stream = modRoots.parallelStream()) {
-            stream.flatMap(IOUtil.uncheckFunction(Files::walk)).forEachOrdered(p -> {
+            stream.flatMap(IOUtil.uncheckFunction(Files::walk)).sorted().forEachOrdered(p -> {
                 try {
                     hos.write(p.toString().getBytes(StandardCharsets.UTF_8));
                     if (Files.isRegularFile(p)) {
