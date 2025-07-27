4593d4517edb6d8680df12c80bb947e6e69096dd
chris <github@onechris.mozmail.com>
2025-07-28 01:16:11 +0200

    Remove sonatype oss maven repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,5 @@ allprojects {
 
         maven("https://maven.fabricmc.net/")
         maven("https://maven.neoforged.net/releases")
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
 }
